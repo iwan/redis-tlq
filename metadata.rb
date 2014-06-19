e71c6f250947861b639cd4d86545cbba61515891
Iwan Buetti <iwan.buetti@gmail.com>
2014-06-19 18:42:11 +0200

    InvalidVersionFormat fixed

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name              "redis-tlq"
 maintainer        "Iwan Buetti"
 maintainer_email  "iwan.buetti+999@gmail.com"
 description       "Installs redis from rwky's ppa"
-version           "0.0.6.1"
+version           "0.0.7"
 
 recipe "redis-tlq", "Installs redis"
 
